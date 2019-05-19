43116c9ab86580050f26a8ad1cb2bae42554b222
ice1203 <furafura43t@gmail.com>
2019-05-19 20:03:06 +0900

    crate new

--- create-function.sh
+++ create-function.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
-  echo "指定された引数は$#個です。" 1>&2
-  echo "実行するには3個の引数が必要です。" 1>&2
-  echo "$0 [LambdaFunctionName] [Role] [AWS CLI Profile]"
-  exit 1
-fi
 FunctionName=$1
 RoleName=$2
-Profile=$3
 AccountID=`aws sts get-caller-identity --profile ${Profile} | jq -r '.Account'`
 DATE=`date +%y%m%d%H%M`
 FILEN=temp_lambdacode_${2}_${DATE}.zip
@@ -18,11 +11,10 @@ aws lambda create-function \
     --zip-file fileb:///tmp/${FILEN} \
     --handler lambda_function.lambda_handler \
     --runtime python3.6 \
-    --role arn:aws:iam::${AccountID}:role/${RoleName} \
+    --role  \
     --layers  ${Layer} \
     --timeout 90 \
     --environment Variables="{EC2_INSTANCEID=test,LINE_CHANNEL_SECRET=test,LINE_CHANNEL_ACCESS_TOKEN=test}" \
-    --region ap-northeast-1 \
-    --profile ${Profile}
+    --region ap-northeast-1
 
 rm -f /tmp/${FILEN}
